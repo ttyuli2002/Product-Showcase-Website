3a2729a5bb1ba7ecf59158e9661111d1383e80f3
Alex Xie <45025485+alexxie2288@users.noreply.github.com>
2024-02-24 16:00:49 -0500

    Update Navbar.js

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -5,10 +5,10 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/">首页</a></li>
+        <li><a href="/about">关于我们</a></li>
+        <li><a href="/services">服务</a></li>
+        <li><a href="/contact">联系方式</a></li>
       </ul>
     </nav>
   );
