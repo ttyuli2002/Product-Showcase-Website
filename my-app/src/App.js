110b7974519fb6eaf1c4eddaeed22b6df1a4849e
Alex Xie <45025485+alexxie2288@users.noreply.github.com>
2024-02-24 15:28:32 -0500

    navigation bar create

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <body class = 'main'>
+          <Navbar />
           <h2>智能驾驶</h2>
+          
         </body >
       </header>
     </div>
