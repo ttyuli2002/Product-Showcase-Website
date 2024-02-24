02154f26bb1b98f9a66e3ee746f76a36fc0a12d1
Alex Xie <45025485+alexxie2288@users.noreply.github.com>
2024-02-24 14:52:53 -0500

    react create

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <body class = 'main'>
+          <h2>智能驾驶</h2>
+        </body >
+      </header>
+    </div>
+  );
+}
+
+export default App;
